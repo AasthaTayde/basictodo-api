5d43e090527dc274fd11426c3543c8b00d378ae7
Aastha <aasthaktayde@gmail.com>
2026-02-03 23:50:31 +0530

    some comments added

--- server.cjs
+++ server.cjs
@@ -1,27 +1,37 @@
 
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-app.use(express.json());
-app.use(express.static("public"));
+require("dotenv").config();//store secret or env-specific values outside your main code.
+const express = require("express");//imports express library
+const mongoose = require("mongoose");//import mongoose library to use in this file
+const app = express();//creates an express application object
+app.use(express.json());//tells Express to automatically parse incoming JSON requests.
+app.use(express.static("public"));// Serve frontend files from "public" folder
 console.log("Server Starting");
 
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
 .then(() => console.log("✅ MongoDB Atlas connected"))
 .catch((err) => console.error("❌ MongoDB connection error:", err));
+//OR
+/*try{
+    await mongoose.connect(MONGO_URI);
+    console.log("DB connected");
+}
+catch(err)
+{
+    console.error(err);
+}*/
+
 
 app.get("/status",(req,res)=>{
     res.json({message:"Server is running",db:mongoose.connection.readyState});
-});
+});//returns a number, 1:connected,0:disconnected.
 
 const todoSchema = new mongoose.Schema({
     title:{ type:String,required:true},
     completed:{type:Boolean,default:false},
-});
+});//creates a mongoose schema for todo item
 
-const Todo=mongoose.model("Todo",todoSchema);
+const Todo=mongoose.model("Todo",todoSchema);//containes in built functions
 
 app.get("/todos", async (req,res)=>{
     try{
@@ -36,7 +46,7 @@ catch(err)
 
 app.post("/todos", async (req, res)=>{
     try{
-        const { title } = req.body;
+        const { title } = req.body;//extracts the title from the incoming JSON body., to convert raw json text into js object
         if (!title) return res.status(400).json({error:"Title is required"});
 
         const newTodo = await Todo.create({title});
